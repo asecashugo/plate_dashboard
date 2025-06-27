e0a5fdcf4577fb2dbef129edb5509b456b5d55d6
Hugo <asecashugo@gmail.com>
2025-06-27 23:45:19 +0200

    fix credentials

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 import streamlit as st
 import boto3
 
-dynamodb = boto3.resource('dynamodb',region_name='eu-north-1')
+aws_access_key = st.secrets["aws"]["aws_access_key_id"]
+aws_secret_key = st.secrets["aws"]["aws_secret_access_key"]
+
+dynamodb = boto3.resource(
+    'dynamodb',
+    region_name='eu-north-1',
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key
+)
+
+# dynamodb = boto3.resource('dynamodb',region_name='eu-north-1')
 table = dynamodb.Table('matriculas')
 
 st.title("🔍 License Plate Tracker")
